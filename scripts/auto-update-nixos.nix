c16e872e8cf082f7323b5d7434cab799f3e42a74
Gambled <ipog71@gmail.com>
2023-11-22 15:46:32 -0600

    auto update Wed Nov 22 03:46:32 PM CST 2023

---
+++ scripts/auto-update-nixos.nix
@@ -0,0 +1,11 @@
+{ pkgs }:
+
+pkgs.writeShellScriptBin "auto-update-nixos" ''
+  #!${pkgs.bash}/bin/bash
+  sudo cp -r /etc/nixos/* /home/gambled/Documents/nix-dotfiles/
+  cd /home/gambled/Documents/nix-dotfiles/
+  sudo git add .
+  sudo git commit -m "auto update $(date)"
+  git push
+  echo "Sistema respaldado! n.n" | ${pkgs.lolcat}/bin/lolcat
+''
