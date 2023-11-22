f2009a577c357a62845b32199ec98a1011a8469e
Gambled <ipog71@gmail.com>
2023-11-22 16:26:54 -0600

    auto update Wed Nov 22 04:26:54 PM CST 2023

--- scripts/auto-update-nixos.nix
+++ scripts/auto-update-nixos.nix
@@ -2,6 +2,8 @@
 
 pkgs.writeShellScriptBin "auto-update-nixos" ''
   #!${pkgs.bash}/bin/bash
+  sudo nixos-rebuild switch 
+  
   sudo cp -r /etc/nixos/* /home/gambled/Documents/nix-dotfiles/
   cd /home/gambled/Documents/nix-dotfiles/
   sudo git add .
