82ae99117356bbfb26c9b968d8655d24334ed4c3
Gambled23 <ipog71@gmail.com>
2024-04-30 12:00:04 -0600

    auto update Tue Apr 30 12:00:04 PM CST 2024

--- devices/dev/configuration.nix
+++ devices/dev/configuration.nix
@@ -12,62 +12,62 @@
     system.userActivationScripts.linktosharedfolder.text = ''
     # Documentos personales
     if [[ ! -h "$HOME/Documents/Documentos personales" ]]; then
-      ln -s "/Documentos/Nextcloud/Documentos personales" "$HOME/Documents/Documentos personales"
+      ln -s "/documentos/Nextcloud/Documentos personales" "$HOME/Documents/Documentos personales"
     fi
 
     # Modular
     if [[ ! -h "$HOME/Documents/Modular" ]]; then
-      ln -s "/Documentos/Nextcloud/Modular" "$HOME/Documents/Modular"
+      ln -s "/documentos/Nextcloud/Modular" "$HOME/Documents/Modular"
     fi
 
     # Wallpapers
     if [[ ! -h "$HOME/Pictures/Wallpapers" ]]; then
-      ln -s "/Documentos/Nextcloud/Wallpapers" "$HOME/Pictures/Wallpapers"
+      ln -s "/documentos/Nextcloud/Wallpapers" "$HOME/Pictures/Wallpapers"
     fi
 
     # Codes
     if [[ ! -h "$HOME/Codes" ]]; then
-      ln -s "/Documentos/Codes/" "$HOME/Codes"
+      ln -s "/documentos/Codes/" "$HOME/Codes"
     fi
 
     # Documents
     if [[ ! -h "$HOME/Documents" ]]; then
-      ln -s "/Documentos/Documents/" "$HOME/Documents"
+      ln -s "/documentos/Documents/" "$HOME/Documents"
     fi
 
     # Downloads
     if [[ ! -h "$HOME/Downloads" ]]; then
-      ln -s "/Documentos/Downloads/" "$HOME/Downloads"
+      ln -s "/documentos/Downloads/" "$HOME/Downloads"
     fi
 
     # Music
     if [[ ! -h "$HOME/Music" ]]; then
-      ln -s "/Documentos/Music/" "$HOME/Music"
+      ln -s "/documentos/Music/" "$HOME/Music"
     fi
 
     # Nextcloud
     if [[ ! -h "$HOME/Nextcloud" ]]; then
-      ln -s "/Documentos/Nextcloud/" "$HOME/Nextcloud"
+      ln -s "/documentos/Nextcloud/" "$HOME/Nextcloud"
     fi
 
     # Pictures
     if [[ ! -h "$HOME/Pictures" ]]; then
-      ln -s "/Documentos/Pictures/" "$HOME/Pictures"
+      ln -s "/documentos/Pictures/" "$HOME/Pictures"
     fi
 
     # Public
     if [[ ! -h "$HOME/Public" ]]; then
-      ln -s "/Documentos/Public/" "$HOME/Public"
+      ln -s "/documentos/Public/" "$HOME/Public"
     fi
 
     # Templates
     if [[ ! -h "$HOME/Templates" ]]; then
-      ln -s "/Documentos/Templates/" "$HOME/Templates"
+      ln -s "/documentos/Templates/" "$HOME/Templates"
     fi
 
     # Videos
     if [[ ! -h "$HOME/Videos" ]]; then
-      ln -s "/Documentos/Videos/" "$HOME/Videos"
+      ln -s "/documentos/Videos/" "$HOME/Videos"
     fi
   '';
 }
