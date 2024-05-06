2f16be7c28daeabf0e43ad8590e342ccc7dfbad9
Gambled23 <ipog71@gmail.com>
2024-05-06 10:31:32 -0600

    auto update Mon May  6 10:31:32 AM CST 2024

--- devices/dev/homeDev.nix
+++ devices/dev/homeDev.nix
@@ -8,7 +8,13 @@
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.11";
   imports = [
-    ../../home.nix
+    ../../core/programs/alacritty.nix
+    ../../core/programs/direnv.nix
+    ../../core/programs/kitty/kitty.nix
+    ../../core/programs/lsd.nix
+    ../../core/programs/starship.nix
+    ../../core/programs/zsh.nix
+    ../../core/programs/firefox.nix
   ];
 
   home = {
@@ -17,8 +23,29 @@
   };
   
   home.packages = with pkgs; [ 
-    # dev languages
+    # dev tools
+    dbeaver
+    vscode
+    kitty
+
+    # messaging
+    vesktop 
+
+    # utils
+    jellyfin-ffmpeg
+    libreoffice
+    autorandr # for multi monitor setups
+
+    # extras
+    bat # cat replacement
+    # oh-my-zsh plugins
+    thefuck # Magnificent app which corrects your previous console command
+    zsh
   ];
+
+  programs.git = {
+    enable = true;
+  };
 }
 
 
