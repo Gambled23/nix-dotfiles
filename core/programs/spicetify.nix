7b4293b79d45e00f6189f7b4528c424d79fcf552
Gambled23 <ipog71@gmail.com>
2024-06-21 16:04:49 -0600

    auto update vie 21 jun 2024 16:04:49 CST

--- core/programs/spicetify.nix
+++ core/programs/spicetify.nix
@@ -11,7 +11,7 @@ in
   programs.spicetify = {
     enable = true;
     
-    theme = spicePkgs.themes.Ziro;
+    theme = spicePkgs.themes.Sleek;
 
     enabledCustomApps = with spicePkgs.apps; [
       marketplace
