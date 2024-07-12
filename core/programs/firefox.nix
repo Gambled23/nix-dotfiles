755e5e9c2076af03182b44a263029a41df147c76
Gambled23 <ipog71@gmail.com>
2024-07-12 14:21:35 -0600

    auto update vie 12 jul 2024 14:21:35 CST

--- core/programs/firefox.nix
+++ core/programs/firefox.nix
@@ -6,7 +6,6 @@
 }: {
   programs.firefox = {
     enable = true;
-/*
     profiles."gambled" = {
       id = 0;
       name = "gambled";
@@ -47,8 +46,33 @@
         "extensions.pocket.showHome" = false;
         "extensions.pocket.site" = "";
       };
+      search = {
+        force = true;
+        default = "Google";
+        order = [ "Google" ];
+        engines = {
+          "Nix Packages" = {
+            urls = [{
+              template = "https://search.nixos.org/packages";
+              params = [
+                { name = "type"; value = "packages"; }
+                { name = "query"; value = "{searchTerms}"; }
+              ];
+            }];
+            icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@np" ];
+          };
+          "NixOS Wiki" = {
+            urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
+            iconUpdateURL = "https://nixos.wiki/favicon.png";
+            updateInterval = 24 * 60 * 60 * 1000; # every day
+            definedAliases = [ "@nw" ];
+          };
+          "Bing".metaData.hidden = true;
+          "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
+        };
+      };
     };
-*/
   };
 
   home.sessionVariables = {
