319d1062fbba960cb6fd0e5ef9602b640fb6db3a
Gambled23 <ipog71@gmail.com>
2024-07-13 14:54:54 -0600

    auto update sáb 13 jul 2024 14:54:54 CST

--- core/services/suwayomi.nix
+++ core/services/suwayomi.nix
@@ -6,9 +6,15 @@ with lib;
   services.suwayomi-server = {
     enable = true;
     openFirewall = true;
+
     settings = {
       server.systemTrayEnabled = true;
-      server.extensionRepos = [ "https://raw.githubusercontent.com/keiyoushi/extensions/repo/index.min.json" ]; 
+      server.extensionRepos = [ "https: //raw.githubusercontent.com/keiyoushi/extensions/repo/index.min.json" ]; 
+      server.webUIEnabled = true;
+      server.initialOpenInBrowserEnabled = true;
+      server.webUIInterface = "browser"; # "browser" or "electron"
+      server.webUIFlavor = "WebUI"; # "WebUI" or "Custom"
+      server.basicAuthEnabled = false;
     };
   };
 }
