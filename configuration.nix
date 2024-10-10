c739750a3ad5150533b0f04cfa6a18c64687634f
Gambled23 <ipog71@gmail.com>
2024-10-10 13:06:08 -0600

    auto update jue 10 oct 2024 13:06:08 CST

--- configuration.nix
+++ configuration.nix
@@ -20,8 +20,9 @@
   ];
 
   boot.extraModprobeConfig = ''
-    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+    options bluetooth disable_ertm=1
   '';
+  
   security.polkit.enable = true;
   
   # set zsh shell
