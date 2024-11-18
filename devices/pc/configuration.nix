4e13df8ce4abd401e3e84b47efe5058bb60eb46b
Gambled23 <ipog71@gmail.com>
2024-11-18 17:08:19 -0600

    auto update lun 18 nov 2024 17:08:19 CST

--- devices/pc/configuration.nix
+++ devices/pc/configuration.nix
@@ -15,9 +15,21 @@
     #../../core/services/suwayomi.nix
 
     # bootloader
-    ../../core/bootloader/systemd.nix
-    #../../core/bootloader/grub.nix
+    #../../core/bootloader/systemd.nix
+    ../../core/bootloader/grub.nix
   ];
+
+  boot.loader.grub.theme = pkgs.stdenv.mkDerivation {
+    pname = "distro-grub-themes";
+    version = "3.1";
+    src = pkgs.fetchFromGitHub {
+      owner = "AdisonCavani";
+      repo = "distro-grub-themes";
+      rev = "v3.1";
+      hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+    };
+    installPhase = "cp -r customize/nixos $out";
+  };
 
   # amdgpu
   boot.initrd.kernelModules = [ "amdgpu" ];
