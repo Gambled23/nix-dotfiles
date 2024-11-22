b763359808934e4a8424443819cb3aa09fd8b533
Gambled23 <ipog71@gmail.com>
2024-11-22 15:56:33 -0600

    auto update vie 22 nov 2024 15:56:33 CST

--- core/services/xrdp.nix
+++ core/services/xrdp.nix
@@ -2,7 +2,7 @@
 
 {
   # xrdp server
-  services.xrdp.enable = true;
-  services.xrdp.defaultWindowManager = "startplasma-x11";
+  #services.xrdp.enable = true;
+  #services.xrdp.defaultWindowManager = "startplasma-x11";
   services.xrdp.openFirewall = true;
 }
