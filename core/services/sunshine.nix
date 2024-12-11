e8dd3882a23744af40faee2b40537e4fc6c9fa3b
Gambled23 <ipog71@gmail.com>
2024-12-11 14:34:26 -0600

    auto update Wed Dec 11 02:34:26 PM CST 2024

--- core/services/sunshine.nix
+++ core/services/sunshine.nix
@@ -6,5 +6,24 @@
     autoStart = true;
     capSysAdmin = true;
     openFirewall = true;
+    applications =
+    {
+      env = {
+        PATH = "$(PATH):$(HOME)/.local/bin";
+      };
+      apps = [
+        {
+          name = "Desktop";
+          image-path = "desktop.png";
+        }
+        {
+          name = "Steam Big Picture";
+          detached = [
+            "modo-tele"
+          ];
+          image-path = "steam.png";
+        }
+      ];
+    };
   };
 }
