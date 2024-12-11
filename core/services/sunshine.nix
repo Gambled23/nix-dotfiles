8507c0f792fbd3cd83698abfc19984e7eeeaada5
Gambled23 <ipog71@gmail.com>
2024-12-11 13:42:27 -0600

    auto update Wed Dec 11 01:42:27 PM CST 2024

--- core/services/sunshine.nix
+++ core/services/sunshine.nix
@@ -5,6 +5,8 @@
     enable = true;
     capSysAdmin = true;
     openFirewall = true;
-    settings.port = 47989;
+    settings.port = 47990;
   };
+  networking.firewall.allowedTCPPortRanges = [{ from = 47984; to = 48010; }];
+  networking.firewall.allowedUDPPortRanges = [{ from = 47988; to = 48010; }];
 }
