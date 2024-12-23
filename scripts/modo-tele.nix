37b895f71bb83595c8da8f8c2ed8ab885eef1c34
Gambled23 <ipog71@gmail.com>
2024-12-23 13:38:57 -0600

    auto update Mon Dec 23 01:38:57 PM CST 2024

--- scripts/modo-tele.nix
+++ scripts/modo-tele.nix
@@ -2,18 +2,25 @@
 
 pkgs.writeShellScriptBin "modo-tele" ''
   #!${pkgs.bash}/bin/bash
-  current_device=$(pactl info | grep "Default Sink")
-  if [[ $current_device == *"Razer"* ]]; then
-    pactl set-default-sink alsa_output.pci-0000_0a_00.1.hdmi-stereo
+
+  if [ $1 == 'enable' ]; then
     ${pkgs.killall}/bin/killall -q steam
     while pgrep steam > /dev/null; do sleep 1; done
     steam -bigpicture
-  else
-    pactl set-default-sink alsa_output.usb-Razer_Razer_Kraken_Ultimate_00000000-00.analog-stereo
+  fi
+
+  if [ $1 == 'disable' ]; then
     ${pkgs.killall}/bin/killall -q steam
     while pgrep steam > /dev/null; do sleep 1; done
     steam
   fi
+
+  if [ "$2" == '-audio' ]; then
+    current_device=$(pactl info | grep "Default Sink")
+    if [[ $current_device == *"SteelSeries"* ]]; then
+      pactl set-default-sink alsa_output.pci-0000_0a_00.1.hdmi-stereo
+    else
+      pactl set-default-sink alsa_output.usb-SteelSeries_Arctis_7_-00.analog-stereo
+    fi
+  fi
 ''
-
-
