2b0c8731b29de45f44981ff105b8776b41786e0b
Gambled23 <ipog71@gmail.com>
2024-12-26 14:04:35 -0600

    auto update Thu Dec 26 02:04:35 PM CST 2024

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    jovian-nixos.url = "github:Jovian-Experiments/Jovian-NixOS";
     #nixpkgs-xr.url = "github:nix-community/nixpkgs-xr";
 
 
@@ -19,7 +20,7 @@
     stylix.url = "github:danth/stylix";
   };
 
-  outputs = { self, nixpkgs, home-manager, plasma-manager, spicetify-nix, kde2nix, stylix, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, plasma-manager, spicetify-nix, kde2nix, stylix, jovian-nixos, ... }@inputs:
     let
     specialArgs = {
       inherit spicetify-nix;
@@ -53,6 +54,7 @@
           ./devices/pc/configuration.nix
           home-manager.nixosModules.home-manager
           stylix.nixosModules.stylix
+          jovian-nixos.nixosModules.default
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
