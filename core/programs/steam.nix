1eb41df436b282fdfd42766f02ea4b9201fb26a0
Gambled23 <ipog71@gmail.com>
2024-12-26 14:50:54 -0600

    auto update Thu Dec 26 02:50:54 PM CST 2024

--- core/programs/steam.nix
+++ core/programs/steam.nix
@@ -1,20 +1,5 @@
 { config, pkgs, lib, ... }:
 let
-  # plugin_loader = pkgs.buildFHSUserEnv {
-  #   name = "PluginLoader";
-  #   targetPkgs = p: with p; [
-  #     zlib
-  #     coreutils
-
-  #     /* needed for css loader */
-  #     curl
-  #     unzip
-
-  #     /* needed for volume mixer */
-  #     pulseaudio
-  #   ];
-  #   runScript = "/home/user/homebrew/services/PluginLoader";
-  # };
   elias = "joto";
 in
 {
@@ -32,23 +17,14 @@ in
 
   programs.gamemode.enable = true;
 
-  # Decky
-  # systemd.services.plugin_loader = {
-  #   wantedBy = [ "multi-user.target" ];
-  #   after = [ "network-online.target" ];
-  #   wants = [ "network-online.target" ];
-  #   environment = {
-  #     PLUGIN_PATH = "/home/user/homebrew/plugins";
-  #     LOG_LEVEL = "INFO";
-  #   };
-  #   serviceConfig = {
-  #     Type = "simple";
-  #     User = "root";
-  #     Restart = "always";
-  #     ExecStart = "${plugin_loader}/bin/PluginLoader";
-  #     WorkingDirectory = "/home/user/homebrew/services";
-  #     KillSignal = "SIGKILL";
-  #   };
-  # };
-  
+  jovian = {
+    # steam = {
+    #   enable = true;
+    #   desktopSession = "plasma-wayland";
+    # };
+    # decky-loader = {
+    #   enable = true;
+    # };
+    hardware.has.amd.gpu = true;
+  };
 }
