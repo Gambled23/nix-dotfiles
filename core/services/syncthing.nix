abdd82b235ee3731164553ef9aaf4779889e5eee
Gambled23 <ipog71@gmail.com>
2025-01-31 16:55:01 -0600

    auto update Fri Jan 31 04:55:01 PM CST 2025

--- core/services/syncthing.nix
+++ core/services/syncthing.nix
@@ -3,6 +3,9 @@
 {
   services.syncthing = {
     enable = true;
+    user = "gambled";
     openDefaultPorts = true;
+    dataDir = "/home/gambled";  # default location for new folders
+    configDir = "/home/gambled/.config/syncthing";
   };
 }
