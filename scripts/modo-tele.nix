6888ee3defe7902fd7db2824147eae9923cfbe50
Gambled23 <ipog71@gmail.com>
2025-04-22 09:27:01 -0600

    auto-update Tue Apr 22 09:27:01 CST 2025

--- scripts/modo-tele.nix
+++ scripts/modo-tele.nix
@@ -4,25 +4,19 @@ pkgs.writeShellScriptBin "modo-tele" ''
   #!${pkgs.bash}/bin/bash
 
   if [ $1 == 'enable' ]; then
-    kscreen-doctor output.HDMI-A-2.enable
-    ${pkgs.killall}/bin/killall -q steam
-    while pgrep steam > /dev/null; do sleep 1; done
-    start-gamescope-session
+    kscreen-doctor output.DP-1.disable output.HDMI-A-1.enable
   fi
 
   if [ $1 == 'disable' ]; then
-    kscreen-doctor output.HDMI-A-2.disable
-    ${pkgs.killall}/bin/killall -q steam
-    while pgrep steam > /dev/null; do sleep 1; done
-    steam
+    kscreen-doctor output.DP-1.enable output.HDMI-A-1.disable
   fi
 
   if [ "$2" == '-audio' ]; then
     current_device=$(pactl info | grep "Default Sink")
-    if [[ $current_device == *"SteelSeries"* ]]; then
+    if [[ $current_device == *"Logitech"* ]]; then
       pactl set-default-sink alsa_output.pci-0000_0a_00.1.hdmi-stereo
     else
-      pactl set-default-sink alsa_output.usb-SteelSeries_Arctis_7_-00.analog-stereo
+      pactl set-default-sink alsa_output.usb-Logitech_PRO_X_Wireless_Gaming_Headset-00.analog-stereo
     fi
   fi
 ''
