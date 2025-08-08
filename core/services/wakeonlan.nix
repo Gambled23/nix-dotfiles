9b35612b06ac2dd49948e8bf64c0b08b9376adda
Gambled23 <ipog71@gmail.com>
2025-08-08 14:12:58 -0600

    auto-update Fri Aug  8 14:12:58 CST 2025

--- core/services/wakeonlan.nix
+++ core/services/wakeonlan.nix
@@ -1,14 +1,14 @@
 { config, pkgs, lib, ... }:
 
 {
-  networking.interfaces.enp6s0.wakeOnLan.enable = true;
+  networking.interfaces.enp8s0.wakeOnLan.enable = true;
   systemd.services.wakeonlan = {
     description = "Reenable wake on lan every boot";
     after = [ "network.target" ];
     serviceConfig = {
       Type = "simple";
       RemainAfterExit = "true";
-      ExecStart = "${pkgs.ethtool}/sbin/ethtool -s enp6s0 wol g";
+      ExecStart = "${pkgs.ethtool}/sbin/ethtool -s enp8s0 wol g";
     };
     wantedBy = [ "default.target" ];
   };
