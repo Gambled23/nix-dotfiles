749a2fbea60b36cf37443178205a4a9359a1713d
Gambled23 <ipog71@gmail.com>
2025-08-07 18:55:36 -0600

    auto-update Thu Aug  7 18:55:36 CST 2025

--- devices/pc/hardware-configuration.nix
+++ devices/pc/hardware-configuration.nix
@@ -36,7 +36,7 @@
   #   options = [ "rw" "uid=1000"];
   # };
 
-  swapDevices = [ { device = "/dev/disk/by-label/swap"; } ];
+  # swapDevices = [ { device = "/dev/disk/by-label/swap"; } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
