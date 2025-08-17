79e77dd47b8f76b1972773a273c181cb4eda604e
Gambled23 <ipog71@gmail.com>
2025-08-17 09:36:23 -0600

    auto-update Sun Aug 17 09:36:23 CST 2025

--- core/programs/steam.nix
+++ core/programs/steam.nix
@@ -1,14 +1,13 @@
 { config, pkgs, lib, ... }:
-let
-  elias = "joto";
-in
 {
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
     dedicatedServer.openFirewall = true;
-    # package = with pkgs; steam.override { extraPkgs = pkgs: [ attr ]; };
+    gamescopeSession.enable = true;
+    # extest.enable = true;
+    # protontricks.enable = true;
     package = pkgs.steam-millennium;
     extraCompatPackages = with pkgs; [
       proton-ge-bin
