14acaf6eb6557f3f353d5a1ff75088542e77832b
Gambled23 <ipog71@gmail.com>
2025-10-16 20:32:12 -0600

    auto-update Thu Oct 16 20:32:12 CST 2025

--- core/programs/nixcord.nix
+++ core/programs/nixcord.nix
@@ -13,46 +13,46 @@
         https://raw.githubusercontent.com/shvedes/discord-gruvbox/refs/heads/main/gruvbox-dark.theme.css
       ];
 
-      plugins = {
-        accountPanelServerProfile.enable = true;
-        betterGifPicker.enable = true;
-        betterNotesBox.enable = true;
-        betterRoleContext.enable = true;
-        betterRoleDot.enable = true;
-        betterUploadButton.enable = true;
-        biggerStreamPreview.enable = true;
-        callTimer.enable = true;
-        fakeNitro.enable = true;
-        fakeProfileThemes.enable = true;
-        # forceOwnerCrown.enable = true;
-        friendsSince.enable = true;
-        fullSearchContext.enable = true;
-        fullUserInChatbox.enable = true;
-        gameActivityToggle.enable = true;
-        implicitRelationships.enable = true;
-        lastFMRichPresence = {
-          enable = true;
-          username = "Gambled23";
-          apiKey = "1542ad92a0c24e9e2e17abdaedd49afc";
-          hideWithSpotify = false;
-          nameFormat = "artist-first";
-          useListeningStatus = true;
-          showLastFmLogo = false;
-        };
-        mentionAvatars.enable = true;
-        # platformIndicators.enable = true;
-        # serverListIndicators.enable = true;
-        # spotifyControls.enable = true;
-        typingIndicator.enable = true;
-        typingTweaks.enable = true;
-        userVoiceShow.enable = true;
-        validReply.enable = true;
-        validUser.enable = true;
-        viewIcons.enable = true;
-        volumeBooster.enable = true;
-        webScreenShareFixes.enable = true;
-        whoReacted.enable = true;
-      };
+      # plugins = {
+      #   accountPanelServerProfile.enable = true;
+      #   betterGifPicker.enable = true;
+      #   betterNotesBox.enable = true;
+      #   betterRoleContext.enable = true;
+      #   betterRoleDot.enable = true;
+      #   betterUploadButton.enable = true;
+      #   biggerStreamPreview.enable = true;
+      #   callTimer.enable = true;
+      #   fakeNitro.enable = true;
+      #   fakeProfileThemes.enable = true;
+      #   # forceOwnerCrown.enable = true;
+      #   friendsSince.enable = true;
+      #   fullSearchContext.enable = true;
+      #   fullUserInChatbox.enable = true;
+      #   gameActivityToggle.enable = true;
+      #   implicitRelationships.enable = true;
+      #   lastFMRichPresence = {
+      #     enable = true;
+      #     username = "Gambled23";
+      #     apiKey = "1542ad92a0c24e9e2e17abdaedd49afc";
+      #     hideWithSpotify = false;
+      #     nameFormat = "artist-first";
+      #     useListeningStatus = true;
+      #     showLastFmLogo = false;
+      #   };
+      #   mentionAvatars.enable = true;
+      #   # platformIndicators.enable = true;
+      #   # serverListIndicators.enable = true;
+      #   # spotifyControls.enable = true;
+      #   typingIndicator.enable = true;
+      #   typingTweaks.enable = true;
+      #   userVoiceShow.enable = true;
+      #   validReply.enable = true;
+      #   validUser.enable = true;
+      #   viewIcons.enable = true;
+      #   volumeBooster.enable = true;
+      #   webScreenShareFixes.enable = true;
+      #   whoReacted.enable = true;
+      # };
     };
   };
 }
