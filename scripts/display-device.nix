bffee05ddcd759ac32363dff5b3d1beee93cadd8
Gambled23 <ipog71@gmail.com>
2025-10-18 21:11:29 -0600

    auto-update Sat Oct 18 21:11:29 CST 2025

--- scripts/display-device.nix
+++ scripts/display-device.nix
@@ -2,37 +2,57 @@
 
 pkgs.writeShellScriptBin "display-device" ''
   #!${pkgs.bash}/bin/bash
+  echo "Display device script"
 
-  # Parse options
-  while getopts "u" opt; do
-    case $opt in
+  display_name=""
+
+  # -d expects an argument, h is help
+  while getopts "hd:" opt; do
+    case "$opt" in
       h)
-        echo "Usage: display [-d] [display_name]"
-        echo "  -d                Set display to specified display_name"
-        echo "  display_name     One of 'pc-gambled', 'steamdeck'"
+        echo "Usage: display-device -d <display_name>"
+        echo "  -d <display_name>   One of 'pc-gambled', 'steamdeck'"
+        echo "  -h                  Show this help"
         exit 0
         ;;
       d)
-        if [ $2 ]; then
-          display_name=$2
-          case $display_name in
-            pc-gambled)
-              kscreen-doctor output.DP-3.mode.3440x1440@180
-              ;;
-            steamdeck)
-              kscreen-doctor output.DP-3.mode.1280x800@60
-              ;;
-            *)
-              echo "Invalid display name: $display_name"
-              exit 1
-              ;;
-          esac
-        fi
-      ;;
-      *)
-        echo "Invalid option: -$opt"
+        display_name="$OPTARG"
+        ;;
+      \?)
+        echo "Invalid option: -$OPTARG" >&2
+        echo "Usage: display-device -d <display_name>"
+        echo "  -d <display_name>   One of 'pc-gambled', 'steamdeck'"
+        echo "  -h                  Show this help"
         exit 1
         ;;
     esac
   done
-''
+
+  shift $((OPTIND - 1))
+
+  if [ -z "$display_name" ]; then
+    echo "No display name provided." >&2
+    usage
+    exit 1
+  fi
+
+  case "$display_name" in
+    pc-gambled)
+      kscreen-doctor output.DP-3.mode.3440x1440@180
+      ;;
+    steamdeck)
+      # shutdown rgb
+      sudo systemctl stop openrgb
+      sudo openrgb
+      openrgb -p HOLA.orp 
+      kscreen-doctor output.DP-3.mode.1280x800@60
+      ;;
+    *)
+      echo "Invalid display name: $display_name" >&2
+      echo "Usage: display-device -d <display_name>"
+      echo "  -d <display_name>   One of 'pc-gambled', 'steamdeck'"
+      echo "  -h                  Show this help"
+      exit 1
+      ;;
+  esac
+'' 
