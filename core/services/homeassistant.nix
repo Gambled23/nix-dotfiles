341476cf21d9c37f1c66bcc81aee909bb06ebbe5
Gambled23 <ipog71@gmail.com>
2025-11-09 21:31:22 -0600

    a

--- core/services/homeassistant.nix
+++ core/services/homeassistant.nix
@@ -1,6 +1,9 @@
 {
   services.home-assistant = {
     enable = true;
+    extraPackages = ps: with ps; [ psycopg2 ];
+      config.recorder.db_url = "postgresql://@/hass";
+    };
     extraComponents = [
       # Components required to complete the onboarding
       "analytics"
@@ -16,6 +19,35 @@
       # Includes dependencies for a basic setup
       # https://www.home-assistant.io/integrations/default_config/
       default_config = {};
+      http = {
+        server_host = "::1";
+        trusted_proxies = [ "::1" ];
+        use_x_forwarded_for = true;
+      };
+    }
+  };
+
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ "hass" ];
+    ensureUsers = [{
+      name = "hass";
+      ensureDBOwnership = true;
+    }];
+  };
+
+  services.nginx = {
+    recommendedProxySettings = true;
+    virtualHosts."home.example.com" = {
+      forceSSL = true;
+      enableACME = true;
+      extraConfig = ''
+        proxy_buffering off;
+      '';
+      locations."/" = {
+        proxyPass = "http://[::1]:8123";
+        proxyWebsockets = true;
+      };
     };
   };
 }
