3ea6a2c1a8723655115ab5218d66448e941de7c3
Gambled23 <ipog71@gmail.com>
2025-11-13 20:34:25 -0600

    nix-rbd switch from pc-gambled

--- core/services/xserver/hyprland/menu/walker.nix
+++ core/services/xserver/hyprland/menu/walker.nix
@@ -24,21 +24,10 @@
           {provider = "calc"; prefix = "=";}
           {provider = "providerlist"; prefix = "_";}
         ];
-        empty = ["desktopapplications" "websearch" "calc" "runner"];
+        empty = ["desktopapplications" "websearch" "calc"];
       };
       keybinds.quick_activate = ["F1" "F2" "F3" "F4" "F5" "F6" "F7" "F8" "F9" "F10" "F11" "F12"];
       resume_last_query = true;
-      theme = "gruvbox-dark-medium";
-    };
-
-    themes = {
-      "gruvbox-dark-medium" = {
-        style = "@define-color window_bg_color #282828;
-@define-color accent_bg_color #98971a;
-@define-color theme_fg_color #ebdbb2;
-@define-color error_bg_color #cc241d;
-@define-color error_fg_color #fbf1c7;";
-      };
     };
   };
 }
