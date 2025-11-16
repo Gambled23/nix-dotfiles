21af7af89a5ea803b1c8e1416f7afed957ed469e
Gambled23 <ipog71@gmail.com>
2025-11-16 10:04:34 -0600

    nix-rbd switch from pc-gambled

--- devices/pc/configuration.nix
+++ devices/pc/configuration.nix
@@ -10,6 +10,7 @@
     #../../core/programs/alvr.nix
     # ../../core/services/coolercontrol.nix
     #../../core/programs/corectrl.nix
+    ../../core/services/docker.nix
     ../../core/services/mysql.nix
     ../../core/services/openrgb.nix
     ../../core/programs/steam.nix
