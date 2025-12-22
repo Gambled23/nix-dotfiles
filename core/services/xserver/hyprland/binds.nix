7ff3385fdef025ab109b18a4c8db7d1ed61ccc2e
Gambled23 <ipog71@gmail.com>
2025-12-22 12:10:12 -0600

    nix-rbd switch from pc-gambled

--- core/services/xserver/hyprland/binds.nix
+++ core/services/xserver/hyprland/binds.nix
@@ -13,7 +13,8 @@
       "$mainMod, J, togglesplit," # dwindle
       "$mainMod, C, exec, $code"
       "$mainMod, V, exec, vicinae vicinae://extensions/vicinae/clipboard/history"
-      "$mainMod, B, exec, bluetoothctl connect 24:95:2F:60:BD:94"
+      "$mainMod, B, exec, vicinae vicinae://extensions/vicinae/bluetooth/devices"
+      # "$mainMod, B, exec, bluetoothctl connect 24:95:2F:60:BD:94"
       "$mainMod, L, exec, hyprlock"
       "$mainMod, N, togglefloating,"
       "$mainMod, M, exit,"
