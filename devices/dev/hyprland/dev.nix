20417085b9cb8cea10fff3c5234bb23822f06443
Gambled23 <ipog71@gmail.com>
2026-01-13 17:25:09 -0600

    ignore

--- devices/dev/hyprland/dev.nix
+++ devices/dev/hyprland/dev.nix
@@ -17,7 +17,7 @@
 
       monitor = [
         "eDP-1,1920x1200@60,0x0,1"
-        "HDMI-A-1, 1920x1080@60, 1920x0, 1"
+        "HDMI-A-1, 1920x1080@60, -1920x0, 1"
         "HDMI-A-1, addreserved, 25, 25, 40, 40"
       ];
       "exec-once" = [
