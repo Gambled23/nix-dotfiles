ae987c194f23082a5b9867f6dd49252089537b32
Gambled23 <ipog71@gmail.com>
2026-04-03 20:47:24 -0600

    nix-rbd switch from pc-gambled

--- devices/pc/home.nix
+++ devices/pc/home.nix
@@ -87,7 +87,7 @@
   home.file.".zshrc".text = "export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.libGL}/lib/:${pkgs.libxkbcommon}/lib/:${pkgs.libx11}/lib/:${pkgs.fontconfig}/lib";
 
   services.flatpak.packages = [
-    # "com.stremio.Stremio"
+    "com.stremio.Stremio"
     "com.artemchep.keyguard"
   ];
 }
