abc1df4076a9af560963cc3db6b061234945c850
Gambled23 <ipog71@gmail.com>
2026-04-05 19:58:22 -0600

    nix-rbd switch from pc-gambled

--- core/services/ollama.nix
+++ core/services/ollama.nix
@@ -5,7 +5,9 @@
     package = pkgs.ollama-rocm; #for amd gpu support
 
     loadModels = [
-      "qwen3"
+      "qwen3-coder"
+      "qwen3.5"
+      "deepseek-r1"
     ];
     syncModels = true; #Synchronize all currently installed models with those declared in services.ollama.loadModels, removing any models that are installed but not currently declared there.
 
