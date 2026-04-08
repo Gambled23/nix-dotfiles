bbbd4af95ce94af43c40a08d3736d0f7676b4b45
Gambled23 <ipog71@gmail.com>
2026-04-08 12:08:04 -0600

    a

--- core/services/slsteam.nix
+++ core/services/slsteam.nix
@@ -7,16 +7,34 @@
     AppIds = [
       2868840 # Slay the Spire 2
       4130120 # Genome Guardian 2
-      2161700 # Persona 3 Reload
       1647550 # NEO: The World Ends with You
       2356780 # Dungeon Clawler
+      1295320 # Can of Wormholes
+      3035120 # Is This Seat Taken
+      3401490 # Replicube
+      2239150 # Thronefall
+      2968420 # PowerWash Simulator 2
+      3110070 # Fever Meme
+      1988550 # To The Core
+      1880620 # Once Upon A KATAMARI
+      3314790 # CloverPit
+      2779120 # Modulus Factory Automation
     ];
     AdditionalApps = [
       2868840 # Slay the Spire 2
       4130120 # Genome Guardian 2
-      2161700 # Persona 3 Reload
       1647550 # NEO: The World Ends with You
       2356780 # Dungeon Clawler
+      1295320 # Can of Wormholes
+      3035120 # Is This Seat Taken
+      3401490 # Replicube
+      2239150 # Thronefall
+      2968420 # PowerWash Simulator 2
+      3110070 # Fever Meme
+      1988550 # To The Core
+      1880620 # Once Upon A KATAMARI
+      3314790 # CloverPit
+      2779120 # Modulus Factory Automation
     ];
     SafeMode = true;
   };
@@ -24,7 +42,7 @@
   programs.mangohud = {
     enable = true;
     settings = {
-      toggle_hud = "F11";
+      toggle hud = "F11";
     };
   };
 }
