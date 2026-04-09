850867eb5ac2ba8fe6a6d8da0d44a57c6b844d2f
Gambled23 <ipog71@gmail.com>
2026-04-09 10:47:21 -0600

    nix-rbd switch from dev-gambled

--- core/services/xserver/hyprland/rules.nix
+++ core/services/xserver/hyprland/rules.nix
@@ -29,8 +29,9 @@
       # Opacity only on focus loss
       # "match:class ^(code)$, opacity 1 0.8"
       "match:title Pixel 9 Pro XL, float on, center on, min_size 336 748, max_size 1344 2992, size 518 1153, keep_aspect_ratio on"
-      "match:title Imagen en imagen, float on, center on, keep_aspect_ratio on"
-
+      "match:title Picture in picture, float on, center on, keep_aspect_ratio on"
+      "match:title Open File, float on, center on"
+      
       # Shadow for floating windows
       "match:float off, no_shadow on"
     ];
