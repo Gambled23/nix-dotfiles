9a3067c88ab4809f12a2808bb71d38dc16f6be61
Gambled23 <ipog71@gmail.com>
2026-04-14 16:48:33 -0600

    a

--- core/services/syncthing.nix
+++ core/services/syncthing.nix
@@ -6,6 +6,10 @@
     user = "gambled";
     openDefaultPorts = true;
     guiAddress = "0.0.0.0:8384";
+    networking.firewall.allowedTCPPorts = [
+      8384
+    ];
+    
     dataDir = "/home/gambled";  # default location for new folders
     configDir = "/home/gambled/.config/syncthing";
 
