860cfd22b1e07d31e050eb090add6c0e03de0401
Gambled23 <ipog71@gmail.com>
2026-04-20 10:31:56 -0600

    nix-rbd switch from dev-gambled

--- core/services/ollama-server.nix
+++ core/services/ollama-server.nix
@@ -8,6 +8,7 @@
       "gemma4:e4b"
       "qwen2.5-coder:3b"
       "qwen2.5-coder:7b"
+      "qwen3.5:35b"
     ];
     syncModels = true; #Synchronize all currently installed models with those declared in services.ollama.loadModels, removing any models that are installed but not currently declared there.
 
