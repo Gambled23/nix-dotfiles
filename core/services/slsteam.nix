2d67f5d6e4419ff496199c5ac3cac9a3d1ce9679
Gambled23 <ipog71@gmail.com>
2026-04-20 15:03:46 -0600

    a

--- core/services/slsteam.nix
+++ core/services/slsteam.nix
@@ -9,14 +9,11 @@
       4130120 # Genome Guardian 2
       1647550 # NEO: The World Ends with You
       2356780 # Dungeon Clawler
-      1295320 # Can of Wormholes
       3035120 # Is This Seat Taken
       3401490 # Replicube
       2239150 # Thronefall
       2968420 # PowerWash Simulator 2
       3110070 # Fever Meme
-      1988550 # To The Core
-      1880620 # Once Upon A KATAMARI
       3314790 # CloverPit
       2779120 # Modulus Factory Automation
       2416450 # MOUSE: P.I. For Hire
@@ -26,14 +23,11 @@
       4130120 # Genome Guardian 2
       1647550 # NEO: The World Ends with You
       2356780 # Dungeon Clawler
-      1295320 # Can of Wormholes
       3035120 # Is This Seat Taken
       3401490 # Replicube
       2239150 # Thronefall
       2968420 # PowerWash Simulator 2
       3110070 # Fever Meme
-      1988550 # To The Core
-      1880620 # Once Upon A KATAMARI
       3314790 # CloverPit
       2779120 # Modulus Factory Automation
       2416450 # MOUSE: P.I. For Hire
