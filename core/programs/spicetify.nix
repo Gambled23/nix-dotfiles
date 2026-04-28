ebd46580a4db2dab61d2b7727529f1d26c1a0d5f
Gambled23 <ipog71@gmail.com>
2026-04-28 10:07:09 -0600

    nix-rbd switch from dev-gambled

--- core/programs/spicetify.nix
+++ core/programs/spicetify.nix
@@ -5,43 +5,40 @@ in
 {
   programs.spicetify = {
     enable = true;
-    theme = {
-      name = "comfy";
+    # theme = {
 
-      src = pkgs.fetchFromGitHub {
-        owner = "Comfy-Themes";
-        repo = "Spicetify";
-        rev = "32ff101e27cfd33d85b7cc587f7f95db6b2df8b0";
-        hash = "sha256-sqvmSXJMLE2in/cB8ZIJE/t4J5D0PKRddWECdYJjgX0=";
-      };
-    };
+    #   name = "comfy";
+
+    #   src = pkgs.fetchFromGitHub {
+    #     owner = "Comfy-Themes";
+    #     repo = "Spicetify";
+    #     rev = "32ff101e27cfd33d85b7cc587f7f95db6b2df8b0";
+    #     hash = "sha256-sqvmSXJMLE2in/cB8ZIJE/t4J5D0PKRddWECdYJjgX0=";
+    #   };
+    # };
 
     enabledCustomApps = with spicePkgs.apps; [
-      # newReleases
-      
-      # marketplace
+      marketplace
+      betterLibrary
     ];
 
     enabledExtensions = with spicePkgs.extensions; [
       adblock
-      aiBandBlocker
       # addToQueueTop
       beautifulLyrics
       # bookmark
       # copyToClipboard
-      coverAmbience
       # goToSong
       # groupSession
       # history
       # lastfm
       loopyLoop
       madeForYouShortcut
-      playingSource
       playNext
       # popupLyrics
       # powerBar
       # savePlaylists
-      sectionMarker
+      # sectionMarker
       # skipOrPlayLikedSongs
       # showQueueDuration
       # shuffle
