50c7ae511082c8393310febe101fbbd45bb2cb49
Gambled23 <ipog71@gmail.com>
2026-05-07 17:50:57 -0600

    refactor: nueva estructura

--- core/services/xrdp.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, lib, ... }:
-
-{
-  # xrdp server
-  services.xrdp.enable = true;
-  services.xrdp.defaultWindowManager = "startplasma-wayland";
-  services.xrdp.openFirewall = true;
-}
