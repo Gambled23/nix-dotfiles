236e21435bfdc5cd2c8408a08520b2041142ecb5
Gambled23 <ipog71@gmail.com>
2026-05-15 07:13:59 -0600

    nix-rbd switch from pc-gambled

--- Features/Ricing/noctalia_colors/nvf.nix
+++ Features/Ricing/noctalia_colors/nvf.nix
@@ -1,18 +1,18 @@
 {
-  base00 = "#262417";
-  base01 = "#3f3c27";
-  base02 = "#393623";
-  base03 = "#706e5c";
-  base04 = "#b6b5af";
-  base05 = "#f3f3f2";
-  base06 = "#f3f3f2";
-  base07 = "#f3f3f2";
+  base00 = "#172527";
+  base01 = "#263e40";
+  base02 = "#22383a";
+  base03 = "#5c706e";
+  base04 = "#afb6b6";
+  base05 = "#f2f3f3";
+  base06 = "#f2f3f3";
+  base07 = "#f2f3f3";
   base08 = "#fd4663";
-  base09 = "#72cc66";
-  base0A = "#5cd6a5";
-  base0B = "#e4d667";
-  base0C = "#a0e996";
-  base0D = "#ece293";
-  base0E = "#96e9c7";
+  base09 = "#666fcc";
+  base0A = "#5ca4d6";
+  base0B = "#67e4d9";
+  base0C = "#969de9";
+  base0D = "#93ece4";
+  base0E = "#96c7e9";
   base0F = "#900017";
 }
