c71200c8649f7c67e3db3cc66e3edcc00aa78918
Gambled23 <ipog71@gmail.com>
2026-06-06 13:29:52 -0600

    nix-rbd switch from pc-gambled

--- Features/Gaming/slsteam.nix
+++ Features/Gaming/slsteam.nix
@@ -11,12 +11,14 @@
       3035120 # Is This Seat Taken
       2968420 # PowerWash Simulator 2
       3110070 # Fever Meme
-      3314790 # CloverPit
-      2779120 # Modulus Factory Automation
-      2416450 # MOUSE: P.I. For Hire
       3265700 # Vampire crawlers
       1147860 # UFO 50
-      2582320 # Mixtape
+      1299460 # Wanderstop
+      2814990 # Screamer
+      2097570 # StarVaders
+      2612700 # S4U CITYPUNK 2011 AND LOVE PUNCH
+      1539140 # STONKS9800 Stock Market Simulator
+      2581950 # Tiny Aquarium
     ];
     AdditionalApps = [
       2868840 # Slay the Spire 2
@@ -25,12 +27,14 @@
       3035120 # Is This Seat Taken
       2968420 # PowerWash Simulator 2
       3110070 # Fever Meme
-      3314790 # CloverPit
-      2779120 # Modulus Factory Automation
-      2416450 # MOUSE: P.I. For Hire
       3265700 # Vampire crawlers
       1147860 # UFO 50
-      2582320 # Mixtape
+      1299460 # Wanderstop
+      2814990 # Screamer
+      2097570 # StarVaders
+      2612700 # S4U CITYPUNK 2011 AND LOVE PUNCH
+      1539140 # STONKS9800 Stock Market Simulator
+      2581950 # Tiny Aquarium
     ];
     SafeMode = true;
   };
