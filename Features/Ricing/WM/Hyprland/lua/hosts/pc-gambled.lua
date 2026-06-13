659e057eb0bb96cc31ef09fee5c926455b0e862a
Gambled23 <ipog71@gmail.com>
2026-06-13 09:24:27 -0600

    nix-rbd switch from pc-gambled

--- Features/Ricing/WM/Hyprland/lua/hosts/pc-gambled.lua
+++ Features/Ricing/WM/Hyprland/lua/hosts/pc-gambled.lua
@@ -1,15 +1,15 @@
 
-hl.monitor({
-    output = "DP-3",
-    mode = "3440x1440@180",
-    position = "0x0",
-    scale = "1",
-})
+-- hl.monitor({
+--     output = "DP-3",
+--     mode = "3440x1440@180",
+--     position = "0x0",
+--     scale = "1",
+-- })
 
-hl.monitor({
-    output = "sunshine",
-    disabled = true,
-})
+-- hl.monitor({
+--     output = "sunshine",
+--     disabled = true,
+-- })
 
 hl.window_rule({
     match = {
@@ -21,5 +21,5 @@ hl.window_rule({
 hl.on("hyprland.start", function()
     hl.exec_cmd("steam %U")
     hl.exec_cmd("hyprctl output create headless sunshine")
-    hl.exec_cmd("display-device -r")
+    hl.exec_cmd("display-device -d pc-gambled")
 end)
