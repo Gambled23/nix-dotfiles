2a24ab9ffa58d333da809c35e131bafb53a821e2
Gambled23 <ipog71@gmail.com>
2026-06-17 21:08:03 -0600

    nix-rbd switch from pc-gambled

--- Features/Ricing/WM/Mangowm/hosts/pc-gambled.nix
+++ Features/Ricing/WM/Mangowm/hosts/pc-gambled.nix
@@ -2,7 +2,7 @@
 {
   wayland.windowManager.mango.settings = {
    monitorrule = [
-      "name:DP-3,width:3440,height:1440,refresh:180,x:0,y:0,vrr:1"
+      "name:DP-3,width:3440,height:1440,refresh:180,x:0,y:0,vrr:0"
     ];
   };
 }
