e2ac18adb7dbab443f1e65e0713e8fdb034eab65
Gambled23 <ipog71@gmail.com>
2026-06-19 08:27:09 -0600

    nix-rbd switch from pc-gambled

--- Features/Ricing/DM/ly.nix
+++ Features/Ricing/DM/ly.nix
@@ -2,7 +2,7 @@
 {
   # Display manager and session
   # services.displayManager.defaultSession = "hyprland-uwsm";
-  services.displayManager.defaultSession = "mango";
+  services.displayManager.defaultSession = "hyprland-uwsm";
   services.displayManager = {
     autoLogin.enable = true;
     autoLogin.user = "gambled";
