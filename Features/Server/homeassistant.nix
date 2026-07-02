7d0e58166f5b7ce272e711e6b3d01b925795d58e
Gambled23 <ipog71@gmail.com>
2026-07-01 20:39:23 -0600

    xd

--- Features/Server/homeassistant.nix
+++ Features/Server/homeassistant.nix
@@ -7,7 +7,7 @@ let
   hyprland_fix = ''
     export XDG_RUNTIME_DIR=/run/user/$(id -u);
     export WAYLAND_DISPLAY=wayland-1;
-    # export HYPRLAND_INSTANCE_SIGNATURE=$(ls -t $XDG_RUNTIME_DIR/hypr | head -n 1)
+    export MANGO_INSTANCE_SIGNATURE=$(ls -t $XDG_RUNTIME_DIR/mango-* | head -n 1)
   '';
 in
 {
