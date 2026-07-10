6e3be2954b2c82681a6632194fdeba6cd898fb31
Gambled23 <ipog71@gmail.com>
2026-07-09 20:33:47 -0600

    a

---
+++ Features/Ricing/Shells/Noctalia/Colors/nvf.nix
@@ -0,0 +1,18 @@
+{
+  base00 = "#1c100d";
+  base01 = "#2a1c19";
+  base02 = "#352723";
+  base03 = "#a88a82";
+  base04 = "#e0bfb6";
+  base05 = "#f6ddd7";
+  base06 = "#f6ddd7";
+  base07 = "#f6ddd7";
+  base08 = "#ffb4ab";
+  base09 = "#e4c460";
+  base0A = "#ffb59f";
+  base0B = "#ffb59f";
+  base0C = "#e4c460";
+  base0D = "#ffb59f";
+  base0E = "#ffb59f";
+  base0F = "#93000a";
+}
