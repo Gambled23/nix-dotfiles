b2bdaba8dcf6378d5b753346a9148974947fd0b9
Gambled23 <ipog71@gmail.com>
2026-07-11 09:49:07 -0600

    nix-rbd switch from pc-gambled

--- Features/Ricing/WM/Mangowm/hosts/pc-gambled.nix
+++ Features/Ricing/WM/Mangowm/hosts/pc-gambled.nix
@@ -1,14 +1,17 @@
 {...}:
 {
   wayland.windowManager.mango.settings = {
-  exec-once = [
-    "spotify"
-    "altus"
-    "steam %U"
-    "sleep 10; systemctl --user restart sunshine"
-  ];
-   monitorrule = [
+    exec-once = [
+      "spotify"
+      "altus"
+      "steam %U"
+      "sleep 10; systemctl --user restart sunshine"
+    ];
+    monitorrule = [
       "name:DP-3,width:3440,height:1440,refresh:180,x:0,y:0,vrr:0"
+    ];
+    bind = [
+      "SUPER+SHIFT,p,spawn,display-device -p pc-gambled"
     ];
   };
 }
