e4460c5ce47a065b7b77851709f5325e07249b34
Gambled23 <ipog71@gmail.com>
2026-08-01 10:45:01 -0600

    nix-rbd switch from pc-gambled

--- Scripts/scrcpy-desktop-mode.nix
+++ Scripts/scrcpy-desktop-mode.nix
@@ -2,7 +2,16 @@
 
 pkgs.writeShellScriptBin "scrcpy-desktop-mode" ''
   #!${pkgs.bash}/bin/bash
-  adb shell settings put global overlay_display_devices 1920x1080/170
+  hostname=$(hostname)
+  case $hostname in
+    pc-gambled)
+      adb shell settings put global overlay_display_devices 3440x1440/280
+    ;;
+    dev-gamled)
+      adb shell settings put global overlay_display_devices 1920x1080/170
+    ;;
+  esac
+  
   DISPLAY_ID=$(scrcpy --list-displays 2>&1 | grep -o -e '--display-id=[0-9]*' | tail -n 1 | cut -d= -f2)
   scrcpy --display-id "$DISPLAY_ID" --mouse=uhid --keyboard=uhid --render-driver=opengl -S -b10M --power-off-on-close
   adb shell settings put global overlay_display_devices none
