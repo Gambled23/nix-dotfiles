24a7b4f1ab93375aeb983cfc75fcbfe123927f27
Gambled23 <ipog71@gmail.com>
2026-09-15 15:41:15 -0600

    nix-rbd switch from dev-gambled

--- modules/features/ricing/shells/noctalia/_Colors/Templates/spotifast.nix
+++ modules/features/ricing/shells/noctalia/_Colors/Templates/spotifast.nix
@@ -4,9 +4,9 @@
     "base": "dark",
     "colors": {
       "window": "{{colors.background.default.hex}}",
-      "panel": "{{colors.surface.default.hex}}",
+      "panel": "{{colors.surface_variant.default.hex}}",
       "surface": "{{colors.surface.default.hex}}",
-      "text": "{{colors.on_primary.default.hex}}",
+      "text": "{{colors.on_surface.default.hex}}",
       "accent": "{{colors.primary.default.hex}}",
       "on_accent": "{{colors.on_primary.default.hex}}",
       "outline": "{{colors.outline.default.hex}}",
